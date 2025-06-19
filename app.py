08f058f437240cbe322e328df927c374adcedef7
Riru254 <franklinmuriru@gmail.com>
2025-06-19 13:59:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, render_template, redirect, flash
-from pymongo import MongoClient
+#from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
 app.secret_key = 'secret123'
 
 # MongoDB config
-client = MongoClient("mongodb+srv://murirufranklinw:p2hzS3RbSr2k6L9s@clusterlearning.ab9rzkd.mongodb.net/?retryWrites=true&w=majority&appName=ClusterLearning") #Connects to Mongo
-db = client["tausit&t"] #Use the database specified
-bookings = db["tausibookings"] #Use collection specified
+#client = MongoClient("mongodb+srv://murirufranklinw:p2hzS3RbSr2k6L9s@clusterlearning.ab9rzkd.mongodb.net/?retryWrites=true&w=majority&appName=ClusterLearning") #Connects to Mongo
+#db = client["tausit&t"] #Use the database specified
+#bookings = db["tausibookings"] #Use collection specified
 
 
 @app.route('/index')
