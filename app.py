d4d063d2a56a46040efda96405ddc6811e765dc6
Riru254 <franklinmuriru@gmail.com>
2025-06-19 15:18:11 +0300

    Updated links and blogs

--- app.py
+++ app.py
@@ -34,29 +34,7 @@ def contact():
 @app.route('/booking', methods=['GET', 'POST'])
 def booking():
     # When User Submits Form
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
-        package = request.form['package']
-        date = request.form['date']
-        notes = request.form['notes']
-
-        if not all (name and email and phone and package and date):
-            flash("Please fill in all required fields.", "danger")
-            return redirect('/booking')
-
-        bookings.insert_one({
-            'name': name,
-            'email': email,
-            'phone': phone,
-            'package': package,
-            'date': date,
-            'notes': notes
-        })
-        flash("Success on booking, we will reach you with more details", "success")
-        return redirect('/booking')
-    return render_template('booking.html')
+    pass
 
 if __name__ == '__main__':
     app.run(debug=False)
